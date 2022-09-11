7d4cd5b2e5c8af0118ef801e212a066a9fe8d5aa
Firat Özcan <firat.oezcan@gmail.com>
2022-09-11 16:17:59 +0200

    Implement deleteMany middleware correctly

--- apps/api/src/middlewares/delete.ts
+++ apps/api/src/middlewares/delete.ts
@@ -11,17 +11,55 @@ import { createUserReadAbility } from "../utils/permissions";
 export const createDeleteManyMiddleware = (model: Prisma.ModelName) => {
   const DeleteManyMiddleware = async (resolverData: ResolverData<Context>, next: NextFn) => {
     const { context, args } = resolverData;
+    const findModel = (model: string) => Prisma.dmmf.datamodel.models.find((prismaModel) => prismaModel.name === model);
+    const prismaModel = findModel(model);
+    if (!prismaModel) throw new Error(`Cannot find prisma model for model "${model}"`);
+    const relationFields = prismaModel.fields.filter((field) => field.kind === "object");
 
     const userAbility = await createUserReadAbility(context);
     const userWhere = accessibleBy(userAbility)[model];
 
-    resolverData.args.where = args.where
-      ? {
-          AND: [args.where, userWhere],
-        }
-      : userWhere;
+    const { _count } = transformFields(graphqlFields(resolverData.info));
 
-    return next();
+    return resolverData.context.prisma.$transaction(
+      async (prisma) => {
+        const relatedWhere = {};
+        relationFields.map((relation) => {
+          const fromField = relation?.relationFromFields?.[0] as string;
+          const condition = args.where[fromField];
+          if (condition) {
+            relatedWhere[fromField] = condition;
+          }
+        });
+        const beforeCreateEntryCount = await prisma[model].count({
+          where: {
+            ...userWhere,
+            ...relatedWhere,
+          },
+        });
+
+        const createdEntries = await prisma[model].deleteMany({
+          ...args,
+          ...(_count && transformCountFieldIntoSelectRelationsCount(_count)),
+        });
+
+        const afterCreateEntryCount = await prisma[model].count({
+          where: {
+            ...userWhere,
+            ...relatedWhere,
+          },
+        });
+        if (beforeCreateEntryCount - createdEntries.count !== afterCreateEntryCount) {
+          throw new ForbiddenError(`Cannot create "${model}" with given data`);
+        }
+        return createdEntries;
+      },
+      {
+        // Setting isolation to Serializable because of a race condition
+        // https://github.com/prisma/prisma/issues/8612#issuecomment-1215739412
+        isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
+      }
+    );
   };
   Object.defineProperty(DeleteManyMiddleware, "name", { value: `DeleteMany${model}Middleware` });
   return DeleteManyMiddleware;
